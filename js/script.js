41c32ecd73e126ac4a3406df25895dc88fe57421
polina.kokrina <p.v.korkina@gmail.com>
2017-02-07 16:16:29 +0500

    Добавила скрипт кнопки Вверх

--- js/script.js
+++ js/script.js
@@ -11,3 +11,49 @@ closeMap.addEventListener("click", function (event) {
   event.preventDefault();
   addressWindow.classList.remove("address-wrap-hidden");
 });
+
+var updownElem = document.getElementById('updown');
+var pageYLabel = 0;
+
+updownElem.onclick = function() {
+  var pageY = window.pageYOffset || document.documentElement.scrollTop;
+
+  switch (this.className) {
+    case 'up':
+      pageYLabel = pageY;
+      window.scrollTo(0, 0);
+      this.className = 'down';
+      break;
+
+    case 'down':
+      window.scrollTo(0, pageYLabel);
+      this.className = 'up';
+  }
+}
+
+window.onscroll = function() {
+
+  var pageY = window.pageYOffset || document.documentElement.scrollTop;
+  var innerHeight = document.documentElement.clientHeight;
+
+  switch (updownElem.className) {
+    case '':
+      if (pageY > innerHeight) {
+        updownElem.className = 'up';
+      }
+      break;
+
+    case 'up':
+      if (pageY < innerHeight) {
+        updownElem.className = '';
+      }
+      break;
+
+    case 'down':
+      if (pageY > innerHeight) {
+        updownElem.className = 'up';
+      }
+      break;
+
+  }
+}
