870c2ae00fb0ce56f2868f5b070ff6d3e81f54e6
polina.kokrina <p.v.korkina@gmail.com>
2017-02-11 17:51:26 +0500

    Добавила таймер обратного отсчета

--- js/script.js
+++ js/script.js
@@ -57,3 +57,37 @@ window.onscroll = function() {
 
   }
 }
+
+function get_timer_652(string_652) {
+  var date_new_652 = string_652;
+  var date_t_652 = new Date(date_new_652);
+  var date_652 = new Date();
+  var timer_652 = date_t_652 - date_652;
+
+  if(date_t_652 > date_652) {
+    var day_652 = parseInt(timer_652/(60*60*1000*24));
+
+    if(day_652 < 10) {day_652 = "0" + day_652;}day_652 = day_652.toString();
+      var hour_652 = parseInt(timer_652/(60*60*1000))%24;
+
+    if(hour_652 < 10) {hour_652 = "0" + hour_652;}hour_652 = hour_652.toString();
+      var min_652 = parseInt(timer_652/(1000*60))%60;
+
+    if(min_652 < 10) {min_652 = "0" + min_652;}min_652 = min_652.toString();var sec_652 = parseInt(timer_652/1000)%60;
+
+    if(sec_652 < 10) {sec_652 = "0" + sec_652;}
+      sec_652 = sec_652.toString(); timethis_652 = day_652 + " : " + hour_652 + " : " + min_652 + " : " + sec_652;
+      $(".result-day").text(day_652);
+      $(".result-hour").text(hour_652);
+      $(".result-minute").text(min_652);
+      $(".result-second").text(sec_652);}
+
+  else {
+    $(".result-day").text("00");
+    $(".result-hour").text("00");
+    $(".result-minute").text("00");
+    $(".result-second").text("00");}
+  }
+
+  function getfrominputs_652(){string_652 = "05/03/2017 00:00"; get_timer_652(string_652);setInterval(function(){get_timer_652(string_652);},1000);}$(document).ready(function(){ getfrominputs_652();});
+
